af36c896d6cc16562e195d2906dcbf5d1cd195b0
Sajid Akhtar <theskillster@gmail.com>
2023-10-21 17:26:17 +0100

    day1

--- python/day 1/day-1.py
+++ python/day 1/day-1.py
@@ -1,5 +1,13 @@
 print("Hello World!")
 
+# Lesson 1 Day 1 - Printing
 print("Day 1 - Python Print Function");
 print("The function is declared like this:");
 print("print('what to print')")
+
+# Lesson 2 Day 1 - Debugging Practice
+## Fix the code below
+
+print(Day 1 - String Manipulation")
+print("String Concatentation is done with "+" sign.")
+    print('e.g. print("HJello " + "world")')
