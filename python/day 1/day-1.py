e660ac5bc2b29d12fb0d5ccdb1634d666394eaaf
Sajid <theskillster@gmail.com>
2023-10-21 17:29:42 +0100

    day1

--- python/day 1/day-1.py
+++ python/day 1/day-1.py
@@ -6,8 +6,15 @@ print("The function is declared like this:");
 print("print('what to print')")
 
 # Lesson 2 Day 1 - Debugging Practice
-## Fix the code below
+## Fix the code below 👇
 
 print(Day 1 - String Manipulation")
-print("String Concatentation is done with "+" sign.")
-    print('e.g. print("HJello " + "world")')
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n."))
