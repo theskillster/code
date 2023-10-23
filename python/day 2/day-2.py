a8599d6d81ce1ccbe1a8114ee6a8d77946e159b3
Sajid <theskillster@gmail.com>
2023-10-23 08:05:30 +0100

    day2

---
+++ python/day 2/day-2.py
@@ -0,0 +1,23 @@
+# Day 2 
+## Data Types
+# Strings integers floats and booleans
+#String
+
+print("Hello"[0]) #use the number in brackets to return the char at that place in the string
+print("Hello"[4]) 
+
+print("123"+"345") #This will concatenate the strings and not do any calculations
+
+#Integer
+
+print(123+345) 
+# 342_456_789 underscores act as commas
+
+#Float
+
+3141.89
+
+#Boolean
+
+True
+False
