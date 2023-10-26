0f1b85b7fd4ae2afe7b56a68988d43f6ab94e028
Sajid <theskillster@gmail.com>
2023-10-26 08:32:53 +0100

    day - 5 project end

--- python/day 5/day-5-project.py
+++ python/day 5/day-5-project.py
@@ -12,21 +12,13 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
-
-
-
-
-
-
-
-
-
-
-
 # result.append(random.choice(letters)) you can use this to append to a list (str or int)
 result = []
 for i in range(nr_letters): # while cycling the range of value (nr_letters):
   result += [random.choice(letters)]   # concatenate to the results list with a random selection from the (letters) list.
+  # Essentially doing:
+    # random_char = random.choice(letters)
+    # password += random_char
 for i in range(nr_numbers):
   result += [random.choice(numbers)]
 for i in range(nr_symbols):
@@ -42,3 +34,17 @@ print(rando_result)
 #   password += char
 
 # print(f"Your password is: {password}")
+
+#Eazy Level
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
+# print(password)
