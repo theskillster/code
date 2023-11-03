3c223eccbdcb3f518721efef55e5b691889e6175
Sajid <theskillster@gmail.com>
2023-11-03 13:39:07 +0000

    day-8-1 pdate

--- python/day 8/day-8-1.py
+++ python/day 8/day-8-1.py
@@ -5,12 +5,15 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text, shift):
+    result=[]
+    for letter in text:
+        temp = alphabet.index(letter)
+        result.append(alphabet[(temp+shift)])
+    print(f"{''.join(result)}")
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
+    #e.g. result
     #print output: "The encoded text is mjqqt"
 
     ##HINT: How do you get the index of an item in a list:
@@ -19,3 +22,5 @@ shift = int(input("Type the shift number:\n"))
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+encrypt(text, shift)
