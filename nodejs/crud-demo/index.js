a3626fb1dc94039cf0fb148f17c7e5cf0314f79f
Sajid <theskillster@gmail.com>
2024-09-21 10:56:14 +0100

    mongodb

--- nodejs/crud-demo/index.js
+++ nodejs/crud-demo/index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-});
+
+
 
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node API');
+    res.send('Hello from Node API Server Update');
   });
+
+
+mongoose.connect("mongodb+srv://admin:UXITXsDKQzhpgCZ5@backenddb.bdp7m.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backendDB")
+.then(() => {
+console.log("Connected to the database!");
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000')
+    });
+})
+.catch(() => {
+console.log("Connection failed!");
+})
