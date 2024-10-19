a237429e93e73d069ae98dfe2815b13dec2c35cf
Redsarg <theskillster@gmail.com>
2024-10-19 14:27:10 +0100

    py2

--- python/code-the-classics-vol1/cavern/cavern.py
+++ python/code-the-classics-vol1/cavern/cavern.py
@@ -219,4 +219,158 @@ class Fruit(GravityActor):
             if self.type == Fruit.EXTRA_HEALTH:
                 game.player.health = min(3, game.player.health + 1)
                 game.play_sound("bonus")
-                
+            elif self.type == Fruit.EXTRA_LIFE:
+                game.player.lives += 1
+                game.play_sound("bonus")
+            else:
+                game.player.score += (self.type + 1)* 100
+                game.play_sound("score")
+        
+            self.time_to_live = 0
+        else:
+            self.time_to_live -= 1
+
+        if self.time_to_live <= 0:
+            game.pops.append(Pop((self.x, self.y - 27), 0))
+
+        anim_frame = str([0, 1, 2, 1][(game.timer // 6) % 4])
+        self.image = "fruit" + str(self.type) + anim_frame
+
+class Player(GravityActor):
+    def __init__(self):
+        super().__init__((0, 0))
+
+        self.lives = 2
+        self.score = 0
+
+    def reset(self):
+        self.pos = (WIDTH / 2, 100)
+        self.vel_y = 0
+        self.direction_x = 1
+        self.fire_timer = 0
+        self.hurt_timer = 100
+        self.health = 3
+        self.blowing_orb = None
+
+    def hit_test(self, other):
+        if self.collidepoint(other.pos) and self.hurt_timer < 0:
+            self.hurt_timer = 200
+            self.health -= 1
+            self.vel_y = -12
+            self.landed = False
+            self.direction_x = other.direction_x
+            if self.health > 0:
+                game.play_sound("ouch", 4)
+            else:
+                game.play_sound("die")
+            return True
+        else:
+            return False
+
+    def update(self):
+        super().update(self.health > 0)
+
+        self.fire_timer -= 1
+        self.hurt_timer -= 1
+
+        if self.landed:
+            self.hurt_timer = min(self.hurt_timer, 100)
+            
+        if self.hurt_timer > 100:
+            if self.health > 0:
+                self.move(self.direction_x, 0, 4)
+            else:
+                if self.top >= HEIGHT*1.5:
+                    self.lives -= 1
+                    self.reset()
+        else:
+            dx = 0
+            if keyboard.left:
+                dx = -1
+            elif keyboard.right:
+                dx = 1
+            
+            if dx != 0:
+                self.direction_x = dx
+
+                if self.fire_timer < 10:
+                    self.move(dx, 0, 4)
+
+            if space_pressed() and self.fire_timer <=0 and len(game.orbs) < 5:
+                x = min(730, max(70, self.x + self.direction_x * 38))
+                y = self.y - 35
+                self.blowing_orb = Orb((x,y), self.direction_x)
+                game.orbs.append(self.blowing_orb)
+                game.play_sound("blow", 4)
+                self.fire_timer = 20
+
+            if keyboard.up and self.vel_y == 0 and self.landed:
+                self.vel_y = -16
+                self.landed = False
+                game.play_sound("jump")
+
+        if keyboard.space:
+            if self.blowing_orb:
+                self.blowing_orb.blown_frames += 4
+                if self.blowing_orb.blown_frames >= 120:
+                    self.blowing_orb = None
+        else:
+            self.blowing_org = None
+
+        self.image = "blank"
+        if self.hurt_timer <= 0 or self.hurt_timer % 2 == 1:
+            dir_index = "1" if self.direction_x > 0 else "0"
+            if self.hurt_timer > 100:
+                if self.health > 0:
+                    self.image = "recoil" + dir_index
+                else:
+                    self.image = "fall" + str((game.timer // 4) % 2)
+            elif self.fire_timer > 0:
+                self.image = "blow" + dir_index
+            elif dx == 0:
+                self.image = "still"
+            else:
+                self.image = "run" + dir_index + str((game.timer // 8) % 4)
+
+class Robot(GravityActor):
+    TYPE_NORMAL = 0
+    TYPE_AGGRESSIVE = 1
+
+    def __init__(self, pos, type):
+        super().__init__(pos)
+
+        self.type = type
+
+        self.speed = randint(1, 3)
+        self.direction_x = 1
+        self.alive = True
+
+        self.change_dir_timer = 0
+        self.fire_timer = 100
+
+    def update(self):
+        super().update()
+
+        self.change_dir_timer -= 1
+        self.fire_timer += 1
+
+        if self.move(self.direction_x, 0, self.speed):
+            self.change_dir_timer = 0
+
+        if self.change_dir_timer <= 0:
+            directions = [-1, 1]
+            if game.player:
+                directions.append(sign(game.player.x - self.x))
+            self.direction_x = choice(directions)
+            self.change_dir_timer = randint(100, 250)
+
+        if self.type == Robot.TYPE_AGGRESSIVE and self.fire_timer >= 24:
+            for orb in game.orbs:
+                if orb.y >= self.top and orb.y < self.bottom and \
+                    abs(orb.x - self.x) < 200:
+                    self.direction_x = sign(orb.x - self.x)
+                    self.fire_timer = 0
+                    break
+        
+        
+
