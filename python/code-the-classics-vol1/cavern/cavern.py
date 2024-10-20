4da794e48b0c3a303ffd98953ccacd40c0c973f2
Sajid Akhtar <theskillster@gmail.com>
2024-10-20 20:23:17 +0100

    cavern master copy

--- python/code-the-classics-vol1/cavern/cavern.py
+++ python/code-the-classics-vol1/cavern/cavern.py
@@ -20,7 +20,7 @@ HEIGHT = 480
 TITLE = "Cavern"
 
 NUM_ROWS = 18
-NUM_CLUMNS = 28
+NUM_COLUMNS = 28
 
 LEVEL_X_OFFSET = 50
 GRID_BLOCK_SIZE = 25
@@ -392,4 +392,111 @@ class Robot(GravityActor):
                 orb.trapped_enemy_trap = self.type
                 game.play_sound("trap", 4)
                 break
+        
+        direction_idx = "1" if self.direction_x > 0 else "0"
+        image = "robot" + str(self.type) + direction_idx
+        if self.fire_timer < 12:
+            image += str(5 + (self.fire_timer // 4))
+        else:
+            image += str(1 + ((game.timer // 4) % 4))
+        self.image = image
 
+class Game:
+    def __init__(self, player=None):
+        self.player = player
+        self.level_colour = -1
+        self.level = -1
+
+        self.next_level()
+
+    def fire_probability(self):
+        return 0.001 + (0.0001 * min(100, self.level))
+    
+    def max_enemies(self):
+        return min((self.level +6) //2, 8)
+    
+    def next_level(self):
+        self.level_colour = (self.level_colour + 1) % 4
+        self.level += 1
+
+        self.grid = LEVELS[self.level % len(LEVELS)]
+
+        self.grid = self.grid + [self.grid[0]]
+
+        self.timer = -1
+
+        if self.player:
+            self.player.reset()
+
+        self.fruits = []
+        self.bolts = []
+        self.enemies = []
+        self.pops = []
+        self.orbs = []
+
+        num_enemies = 10 + self.level
+        num_strong_enemies = 1 + int(self.level /1.5)
+        num_weak_enemies = num_enemies - num_strong_enemies
+
+        self.pending_enemies = num_strong_enemies * [Robot.TYPE_AGGRESSIVE] + num_weak_enemies * [Robot.TYPE_NORMAL]
+
+        shuffle(self.pending_enemies)
+
+        self.play_sound("level", 1)
+
+    def get_robot_spawn_x(self):
+        r = randint(0,NUM_COLUMNS-1)
+
+        for i in range(NUM_COLUMNS):
+            grid_x = (r+i) % NUM_COLUMNS
+            if self.grid[0][grid_x] == ' ':
+                return GRID_BLOCK_SIZE * grid_x + LEVEL_X_OFFSET + 12
+        
+        return WIDTH/2
+    
+    def update(self):
+        self.timer += 1
+
+        for obj in self.fruits + self.bolts + self.enemies + self.pops + [self.player] + self.orbs:
+            if obj:
+                obj.update()
+
+        self.fruits = [f for f in self.fruits if f.time_to_live > 0]
+        self.bolts = [b for b in self.bolts if b.active]
+        self.enemies = [e for e in self.enemies if e.alive]
+        self.pops = [p for p in self.pops if p.timer < 12]
+        self.orbs = [o for o in self.orbs if o.timer < 250 and o.y > -40]
+
+        if self.timer % 100 == 0 and len(self.pending_enemies + self.enemies) > 0:
+            self.fruits.append(Fruit((randint(70, 730), randint(75, 400))))
+
+        if self.timer % 81 == 0 and len(self.pending_enemies) > 0 and len(self.enemies) < self.max_enemies():
+            robot_type = self.pending_enemies.pop()
+            pos = (self.get_robot_spawn_x(), -30)
+            self.enemies.append(Robot(pos, robot_type))
+
+        if len(self.pending_enemies + self.fruits + self.enemies + self.pops) == 0:
+            if len([orb for orb in self.orbs in orb.trapped_enemy_type != None]) == 0:
+                self.next_level()
+
+    def draw(self):
+        screen.blit("bg%d" % self.level_colour, (0, 0))
+
+        block_sprite = "block" + str(self.level % 4)
+
+        for row_y in range(NUM_ROWS):
+            row = self.grid[row_y]
+            if len(row) > 0:
+                x = LEVEL_X_OFFSET
+                for block in row:
+                    if block != ' ':
+                        screen.blit(block_sprite, (x, row_y * GRID_BLOCK_SIZE))
+                    x += GRID_BLOCK_SIZE
+
+        all_objs = self.fruits + self.bolts + self.enemies + self.pops + self.orbs
+        all_objs.append(self.player)
+        for obj in all_objs:
+            if obj:
+                obj.draw()
+
+    
